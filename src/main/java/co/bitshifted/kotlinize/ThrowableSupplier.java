292687d23d8d6d4246495b12120ae35e277a1d6d
Vladimir Djurovic <vdjurovic@bitshifted.co>
2026-02-18 12:17:47 +0100

    fix license and documentation

--- src/main/java/co/bitshifted/kotlinize/ThrowableSupplier.java
+++ src/main/java/co/bitshifted/kotlinize/ThrowableSupplier.java
@@ -1,5 +1,17 @@
+/*
+ * Copyright © 2025, Bitshift <https://bitshifted.co>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package co.bitshifted.kotlinize;
 
+/**
+ * Implementation of {@code Supplier} interface that accepts function blocks which  throw checked exception.
+ *
+ * @param <T> return type of supplier
+ */
 @FunctionalInterface
 public interface ThrowableSupplier<T> {
   T get() throws Throwable;
